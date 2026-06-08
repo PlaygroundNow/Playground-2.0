ec501faa7c3aa3bf6ba6ece81f3e31eeb94b7f5c
Ryan Westerberg <ryanwesterberg@gmail.com>
2026-06-08 10:29:52 -1000

    snapshot of blocks

--- utils/repo.ts
+++ utils/repo.ts
@@ -1,10 +1,10 @@
-import { Repo } from "npm:@automerge/automerge-repo";
-import { WebSocketClientAdapter } from "npm:@automerge/automerge-repo-network-websocket";
-import { NodeFSStorageAdapter } from "npm:@automerge/automerge-repo-storage-nodefs";
+import { Repo } from "npm:@automerge/automerge-repo"
+import { WebSocketClientAdapter } from "npm:@automerge/automerge-repo-network-websocket"
+import { NodeFSStorageAdapter } from "npm:@automerge/automerge-repo-storage-nodefs"
 
 const repo = new Repo({
   storage: new NodeFSStorageAdapter(),
   network: [new WebSocketClientAdapter("wss://sync.playground.now")],
-});
+})
 
-export default repo;
+export default repo
